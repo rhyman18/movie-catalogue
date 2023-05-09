db744b271836e8986d8c756619c6e1993cff8cf1
Ari Budiman <rhyman18@gmail.com>
2023-05-09 17:28:03 +0700

    fix(webscocket): turn off

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,9 +3,9 @@ import '../styles/style.css';
 import '../styles/responsive.css';
 import App from './views/app';
 import swRegister from './utils/sw-register';
-import WebSocketInitiator from './utils/websocket-initiator';
+// import WebSocketInitiator from './utils/websocket-initiator';
 import FooterToolsInitiator from './utils/footer-tools-initiator';
-import CONFIG from './global/config';
+// import CONFIG from './global/config';
 
 const app = new App({
   button: document.querySelector('#hamburgerButton'),
@@ -20,7 +20,7 @@ window.addEventListener('hashchange', () => {
 window.addEventListener('load', async () => {
   app.renderPage();
   await swRegister();
-  WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
+  // WebSocketInitiator.init(CONFIG.WEB_SOCKET_SERVER);
 
   // Initialize footer tools
   FooterToolsInitiator.init({
